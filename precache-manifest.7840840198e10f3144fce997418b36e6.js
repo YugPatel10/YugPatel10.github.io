83aadafef3a66659019f7383421d3bef5bfec262
Yug Patel <yugpatel1699@gmail.com>
2020-12-30 19:08:26 +0530

    Updates

---
+++ precache-manifest.7840840198e10f3144fce997418b36e6.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11a89dd108234e71d7c2edc0f2bc5352",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91b916955ec806217628",
+    "url": "/static/css/main.995cbcea.chunk.css"
+  },
+  {
+    "revision": "2ef8e949f46d2df2c302",
+    "url": "/static/js/2.0f163eeb.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.0f163eeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91b916955ec806217628",
+    "url": "/static/js/main.48cfbaaa.chunk.js"
+  },
+  {
+    "revision": "b6d7cbb12fbe2aa8495c",
+    "url": "/static/js/runtime-main.bc1b9153.js"
+  },
+  {
+    "revision": "dfd00ea34cafae982bb21b20a73b86e9",
+    "url": "/static/media/DBMS.dfd00ea3.webp"
+  },
+  {
+    "revision": "bf989778565c49da91f9e0ef13dbdc5c",
+    "url": "/static/media/background.bf989778.webp"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "/static/media/background.e95d0563.webp"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/static/media/bootstrap-4.ffded238.svg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "34c48d56d11f48166bb26696d0f00057",
+    "url": "/static/media/dav.34c48d56.webp"
+  },
+  {
+    "revision": "bcf22073c9f13bab1761b90bce9fec55",
+    "url": "/static/media/django.bcf22073.svg"
+  },
+  {
+    "revision": "0fd157a7626cd501deeef4251ed71d1a",
+    "url": "/static/media/drd.0fd157a7.webp"
+  },
+  {
+    "revision": "c6bab64b183f96aa47077fd3db37afd0",
+    "url": "/static/media/express.c6bab64b.svg"
+  },
+  {
+    "revision": "650372958f7aa4d041c945b5687f9571",
+    "url": "/static/media/foodfrenzy.65037295.webp"
+  },
+  {
+    "revision": "1190a19ce3cc940741a60137a1e074cb",
+    "url": "/static/media/git-icon.1190a19c.svg"
+  },
+  {
+    "revision": "0ff1a6f08182be4aa63a0b54da1b9e57",
+    "url": "/static/media/github.0ff1a6f0.svg"
+  },
+  {
+    "revision": "b38ff61aa327fbdc24f7b1dad7d04d1a",
+    "url": "/static/media/heroku.b38ff61a.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/static/media/html-5.676e8188.svg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "09749cc5d8a7ee75d2774fe2190271b9",
+    "url": "/static/media/linkedin.09749cc5.webp"
+  },
+  {
+    "revision": "dd49e6891f73ed5bb09750491576f979",
+    "url": "/static/media/logow.dd49e689.webp"
+  },
+  {
+    "revision": "82291019f34ab54a677c456a84127cc3",
+    "url": "/static/media/material-ui-1.82291019.svg"
+  },
+  {
+    "revision": "2985235df31b8742d893d5b7e80145c5",
+    "url": "/static/media/mongodb.2985235d.svg"
+  },
+  {
+    "revision": "54d7bddcd3019ec3c3b23db00160ccef",
+    "url": "/static/media/mssql.54d7bddc.svg"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/static/media/nodejs.a1231528.svg"
+  },
+  {
+    "revision": "d7c985e7cca077dc2d77f89a66573e35",
+    "url": "/static/media/postgresql.d7c985e7.svg"
+  },
+  {
+    "revision": "a50f1d7e6ab4bc62c2655b25489edee5",
+    "url": "/static/media/profile.a50f1d7e.webp"
+  },
+  {
+    "revision": "146716bdf1a351db5705ec1e6a25ed08",
+    "url": "/static/media/python.146716bd.svg"
+  },
+  {
+    "revision": "83fe61b05935f090b671b08aeb77b63d",
+    "url": "/static/media/react-bootstrap.83fe61b0.svg"
+  },
+  {
+    "revision": "024196716802e272adb3cbefb81fd557",
+    "url": "/static/media/react-router.02419671.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "f8eea0e1155ea27ade48abbcfcc66ea5",
+    "url": "/static/media/rfid.f8eea0e1.webp"
+  },
+  {
+    "revision": "b446ca5a397c1ef6fb94cd02c4e94335",
+    "url": "/static/media/sass-1.b446ca5a.svg"
+  },
+  {
+    "revision": "6667744521d4eccc4b7a924004a9e7f7",
+    "url": "/static/media/slide1.66677445.webp"
+  },
+  {
+    "revision": "43f24f408377d3f896ef40e60f502ccd",
+    "url": "/static/media/slide2.43f24f40.webp"
+  },
+  {
+    "revision": "a7da8131ef56aa5dd5c747f7b6970f38",
+    "url": "/static/media/slide3.a7da8131.webp"
+  },
+  {
+    "revision": "baa72f637a33abccccab9496e8f82c3c",
+    "url": "/static/media/smartHomeAutomation.baa72f63.webp"
+  },
+  {
+    "revision": "e79a42b9281a3b30f1302d5452f9884f",
+    "url": "/static/media/styled-components.e79a42b9.svg"
+  },
+  {
+    "revision": "3de182d2120823fe49c27eeb1765c53a",
+    "url": "/static/media/typescript.3de182d2.svg"
+  }
+]);
